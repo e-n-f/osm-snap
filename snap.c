8b62a7bfbe32e51ce2283f24d6314cf5f82f879e
Eric Fischer <enf@pobox.com>
2013-07-02 10:42:33 -0700

    Clean up unneeded cruft

--- snap.c
+++ snap.c
@@ -10,34 +10,11 @@
 #include <math.h>
 #include <time.h>
 
-#define TMPFILE "/tmp/osm-tmp-node2"
-
 struct node {
 	unsigned id; // still just over 2^31
-	unsigned date; // not 2038 yet!
 	int lat;
 	int lon;
 };
-
-double dist(double lat1, double lon1, double lat2, double lon2) {
-	double r = 6367 / 1.609344;
-	double foot = 180 / r / 5280 / M_PI;
-
-	lat1 *= M_PI / 180;
-	lon1 *= M_PI / 180;
-	lat2 *= M_PI / 180;
-	lon2 *= M_PI / 180;
-
-        double latd = lat2 - lat1;
-        double lond = (lon2 - lon1) * cos((lat1 + lat2) / 2);
-
-        double d = sqrt(latd * latd + lond * lond) * 180 / M_PI / foot;
-        if (d >= 1000) {
-                d = acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon2 - lon1)) * r * 5280;
-        }
-
-        return d;
-}
 
 int nodecmp(const void *v1, const void *v2) {
 	const struct node *n1 = v1;
@@ -60,7 +37,6 @@ void *search(const void *key, const void *base, size_t nel, size_t width,
 	long long high = nel, low = -1, probe;
 	while (high - low > 1) {
 		probe = (low + high) >> 1;
-		// printf("try %d\n", probe);
 		int c = cmp(((char *) base) + probe * width, key);
 		if (c > 0) {
 			high = probe;
@@ -80,93 +56,31 @@ void *search(const void *key, const void *base, size_t nel, size_t width,
 // http://marcomaggi.github.io/docs/expat.html#overview-intro
 // Copyright 1999, Clark Cooper
 
-#ifdef XML_LARGE_SIZE
-#  if defined(XML_USE_MSC_EXTENSIONS) && _MSC_VER < 1400
-#    define XML_FMT_INT_MOD "I64"
-#  else
-#    define XML_FMT_INT_MOD "ll"
-#  endif
-#else
-#  define XML_FMT_INT_MOD "l"
-#endif
-
 #define BUFFSIZE        8192
 
+char tmpfname[L_tmpnam];
 FILE *tmp;
 void *map = NULL;
 long long nel;
 
-unsigned int oldway = 0;
-double olddist = 0;
-
 unsigned theway = 0;
-unsigned thedate = 0;
 struct node *thenodes[100000];
 unsigned thenodecount = 0;
-char *theuser = NULL;
-char *theuid = NULL;
-char *theversion = NULL;
-char *thetimestamp = NULL;
-int bogus = 0;
-
-unsigned preseq = 0;
-unsigned seq = 0;
 
 char tags[50000] = "";
 
-int olat = 0;
-int olon = 0;
-unsigned oid = 0;
-
-struct node prevnode;
-
-time_t parsedate(const char *date) {
-	struct tm tm;
-
-	if (sscanf(date, "%d-%d-%dT%d:%d:%d",
-		&tm.tm_year, &tm.tm_mon, &tm.tm_mday,
-		&tm.tm_hour, &tm.tm_min, &tm.tm_sec) == 6) {
-
-		int day = tm.tm_mday + (31 * (tm.tm_mon - 1)) + (372 * (tm.tm_year - 2000));
-		return (day * 86400) +
-			3600 * tm.tm_hour + 60 * tm.tm_min + tm.tm_sec;
-	} else {
-		fprintf(stderr, "can't parse date %s\n", date);
-		return 0;
-	}
-}
-
-char *getdate(time_t t) {
-	static char s[] = "2001-01-01 11:11:11";
-
-	int hour = t % 86400;
-	int day = t / 86400;
-
-	int year = day / 372 + 2000;
-	int mon = (day % 372) / 31 + 1;
-	int mday = day % 31;
-
-	int sec = hour % 60;
-	int min = (hour % 3600) / 60;
-	int hr = hour / 3600;
-
-	sprintf(s, "%04d-%02d-%02d %02d:%02d:%02d", year, mon, mday, hr, min, sec);
-	return s;
-}
-
 static void XMLCALL start(void *data, const char *element, const char **attribute) {
+	static struct node prevnode = { 0, 0, 0 };
+
 	if (strcmp(element, "node") == 0) {
 		struct node n;
 		int i;
 		n.id = 0;
-		n.date = 0;
 		n.lat = INT_MIN;
 		n.lon = INT_MIN;
 
 		for (i = 0; attribute[i] != NULL; i += 2) {
-			if (strcmp(attribute[i], "timestamp") == 0) {
-				n.date = parsedate(attribute[i + 1]);
-			} else if (strcmp(attribute[i], "id") == 0) {
+			if (strcmp(attribute[i], "id") == 0) {
 				n.id = atoi(attribute[i + 1]);
 			} else if (strcmp(attribute[i], "lat") == 0) {
 				n.lat = atof(attribute[i + 1]) * 1000000.0;
@@ -175,34 +89,22 @@ static void XMLCALL start(void *data, const char *element, const char **attribut
 			}
 		}
 
-		if (n.id > seq + 10000) {
-			fprintf(stderr, "%u\r", n.id);
-			seq = n.id;
-		}
-
 		if (nodecmp(&n, &prevnode) < 0) {
-			fprintf(stderr, "node went backwards (%d):\n",
+			fprintf(stderr, "node went backwards (%d): ",
 				nodecmp(&n, &prevnode));
-			fprintf(stderr, "%u %u %d %d\n", prevnode.id, prevnode.date, prevnode.lat, prevnode.lon);
-			fprintf(stderr, "%u %u %d %d\n", n.id, n.date, n.lat, n.lon);
+			fprintf(stderr, "%u %d %d to ", prevnode.id, prevnode.lat, prevnode.lon);
+			fprintf(stderr, "%u %d %d\n", n.id, n.lat, n.lon);
 		} else {
-			if (n.id != oid || n.lat != olat || n.lon != olon) {
-				fwrite(&n, sizeof(struct node), 1, tmp);
-
-				oid = n.id;
-				olat = n.lat;
-				olon = n.lon;
-			}
+			fwrite(&n, sizeof(struct node), 1, tmp);
+			prevnode = n;
 		}
-
-		prevnode = n;
 	} else if (strcmp(element, "way") == 0) {
 		if (map == NULL) {
 			fflush(tmp);
 
-			int fd = open(TMPFILE, O_RDONLY);
+			int fd = open(tmpfname, O_RDONLY);
 			if (fd < 0) {
-				perror(TMPFILE);
+				perror(tmpfname);
 				exit(EXIT_FAILURE);
 			}
 
@@ -226,26 +128,12 @@ static void XMLCALL start(void *data, const char *element, const char **attribut
 		}
 
 		int i;
-		theuser = NULL;
-		theuid = NULL;
-		theversion = NULL;
-		thetimestamp = NULL;
 		thenodecount = 0;
-		bogus = 0;
 		strcpy(tags, "");
 
 		for (i = 0; attribute[i] != NULL; i += 2) {
 			if (strcmp(attribute[i], "id") == 0) {
 				theway = atoi(attribute[i + 1]);
-			} else if (strcmp(attribute[i], "user") == 0) {
-				theuser = strdup(attribute[i + 1]);
-			} else if (strcmp(attribute[i], "uid") == 0) {
-				theuid = strdup(attribute[i + 1]);
-			} else if (strcmp(attribute[i], "version") == 0) {
-				theversion = strdup(attribute[i + 1]);
-			} else if (strcmp(attribute[i], "timestamp") == 0) {
-				thetimestamp = strdup(attribute[i + 1]);
-				thedate = parsedate(attribute[i + 1]);
 			}
 		}
 	} else if (strcmp(element, "nd") == 0) {
@@ -253,7 +141,6 @@ static void XMLCALL start(void *data, const char *element, const char **attribut
 
 		struct node n;
 		n.id = 0;
-		n.date = thedate;
 
 		for (i = 0; attribute[i] != NULL; i += 2) {
 			if (strcmp(attribute[i], "ref") == 0) {
@@ -263,14 +150,12 @@ static void XMLCALL start(void *data, const char *element, const char **attribut
 
 		struct node *find = search(&n, map, nel, sizeof(struct node), nodecmp);
 
-		if (find->lat == INT_MIN || find->lat == 0) {
-			printf("FAIL looked for %u found %u %d\n", n.id, find->id, find->lat);
-			bogus = 1;
+		if (find->lat == INT_MIN) {
+			fprintf(stderr, "FAIL looked for %u found %u %d\n", n.id, find->id, find->lat);
 		} else if (find->id == n.id) {
 			thenodes[thenodecount++] = find;
 		} else {
-			printf("FAIL looked for %u found %u\n", n.id, find->id);
-			bogus = 1;
+			fprintf(stderr, "FAIL looked for %u found %u\n", n.id, find->id);
 		}
 	} else if (strcmp(element, "tag") == 0) {
 		if (theway != 0) {
@@ -289,18 +174,7 @@ static void XMLCALL start(void *data, const char *element, const char **attribut
 
 			int n = strlen(tags);
 			if (n + strlen(key) + strlen(value) + 5 < sizeof(tags)) {
-				sprintf(tags + n, ";%s=%s ", key, value);
-			}
-		}
-	} else if (strcmp(element, "changeset") == 0) {
-		int i;
-		for (i = 0; attribute[i] != NULL; i += 2) {
-			if (strcmp(attribute[i], "id") == 0) {
-				unsigned id = atoi(attribute[i + 1]);
-				if (id > preseq + 100000) {
-					fprintf(stderr, "--- changeset %u\n", id);
-					preseq = id;
-				}
+				sprintf(tags + n, ";%s=%s", key, value);
 			}
 		}
 	}
@@ -308,91 +182,54 @@ static void XMLCALL start(void *data, const char *element, const char **attribut
 
 static void XMLCALL end(void *data, const char *el) {
 	if (strcmp(el, "way") == 0) {
-		printf("%u ", theway);
-		//printf("%s ", thetimestamp);
-
-		double sum = 0;
-		int maxdate = 0;
 		int i;
-
-		for (i = 0; i < thenodecount; i++) {
-			if (thenodes[i]->date > maxdate) {
-				maxdate = thenodes[i]->date;
-			}
-		}
-
-		if (thedate > maxdate) {
-			printf("%s ", getdate(thedate));
-		} else {
-			printf("%s ", getdate(maxdate));
-		}
-
-		printf("%s ", getdate(thedate));
-		printf("%s ", getdate(maxdate));
-		printf("%s ", theversion);
-
-		for (i = 1; i < thenodecount; i++) {
-			sum += dist(thenodes[i - 1]->lat / 1000000.0,
-				    thenodes[i - 1]->lon / 1000000.0,
-				    thenodes[i    ]->lat / 1000000.0,
-				    thenodes[i    ]->lon / 1000000.0);
-		}
-
-		printf("%.3f ", sum);
-
 		for (i = 0; i < thenodecount; i++) {
 			printf("%lf,%lf ", thenodes[i]->lat / 1000000.0,
 					   thenodes[i]->lon / 1000000.0);
 		}
 
-		printf("%s ", tags);
-
-		printf("\n");
-
+		printf("// id=%u", theway);
+		printf("%s\n", tags);
 		theway = 0;
-		free(theuser); theuser = NULL;
-		free(theuid); theuid = NULL;
-		free(thetimestamp); thetimestamp = NULL;
-		free(theversion); theversion = NULL;
 	}
 }
 
 int main(int argc, char *argv[]) {
-	putenv("TZ=GMT");
+	if (tmpnam(tmpfname) == NULL) {
+		perror(tmpfname);
+		exit(EXIT_FAILURE);
+	}
 
-	tmp = fopen(TMPFILE, "w");
+	tmp = fopen(tmpfname, "w");
 	if (tmp == NULL) {
-		perror(TMPFILE);
+		perror(tmpfname);
 		exit(1);
 	}
 
 	XML_Parser p = XML_ParserCreate(NULL);
-	if (! p) {
+	if (p == NULL) {
 		fprintf(stderr, "Couldn't allocate memory for parser\n");
-		exit(-1);
+		exit(EXIT_FAILURE);
 	}
 
 	XML_SetElementHandler(p, start, end);
 
-	for (;;) {
-		int done;
+	int done = 0;
+	while (!done) {
 		int len;
 		char Buff[BUFFSIZE];
 
-		len = (int)fread(Buff, 1, BUFFSIZE, stdin);
+		len = fread(Buff, 1, BUFFSIZE, stdin);
 		if (ferror(stdin)) {
        			fprintf(stderr, "Read error\n");
-			exit(-1);
+			exit(EXIT_FAILURE);
 		}
 		done = feof(stdin);
 
 		if (XML_Parse(p, Buff, len, done) == XML_STATUS_ERROR) {
-			fprintf(stderr, "Parse error at line %" XML_FMT_INT_MOD "u:\n%s\n", XML_GetCurrentLineNumber(p), XML_ErrorString(XML_GetErrorCode(p)));
-			exit(-1);
+			fprintf(stderr, "Parse error at line %lld:\n%s\n", (long long) XML_GetCurrentLineNumber(p), XML_ErrorString(XML_GetErrorCode(p)));
+			exit(EXIT_FAILURE);
 		}
-
-	   	if (done)
-     			break;
 	}
 
 	XML_ParserFree(p);
